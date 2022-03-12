ab60a52e6d0d1b9f3b5c4311777ed79b32d93fb2
Pawan Saket <pawan@dentira.com>
2022-03-13 00:04:09 +0530

    Changing port to 8000

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { app } from './app';
 import { DatabaseConnectionError } from './error-handlers/database-connection-error';
 import { connectToDatabase } from './mongohelper';
 
-const port = process.env.NODE_ENV !== 'production' ? process.env.PORT : 8080;
+const port = process.env.PORT || 8000;
 
 
 
@@ -23,7 +23,7 @@ const start = async () => {
         console.log(err);
         throw new DatabaseConnectionError();
     }
-    app.listen(port, () => console.log("Giter app is listening on the port 3000!!!!!!!"));
+    app.listen(port, () => console.log(`Giter app is listening on the port ${port}!!!!!!!`));
 }
 
 start();
